7ff59d6a7cb77afb2efe9ccb8b2046aa88c27bc8
Christian Salas <christiansalas@Christians-MBP.hsd1.il.comcast.net>
2020-12-19 14:42:57 -0600

    initial commit

---
+++ day1.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class day1 {
+    /*
+    1721
+    979
+    366
+    299
+    675
+    1456
+    */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        List<Integer> lst = new ArrayList<>();
+        // functions are modular for target (although for this problem it will always be 2020)
+        while (s.hasNextInt()) {
+            lst.add(s.nextInt());
+        }
+        System.out.println(DayOneP1(lst));
+        System.out.println(DayOneP2(lst));
+        s.close();
+    }
+
+    public static int DayOneP2(List<Integer> nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            HashSet<Integer> map = new HashSet<>();
+            int curr = 2020 - nums.get(i);
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (map.contains(curr - nums.get(j))) {
+                    ans = nums.get(i) * nums.get(j) * (curr - nums.get(j));
+                }
+                map.add(nums.get(j));
+            }
+        }
+        return ans;
+    }
+
+    public static int DayOneP1(List<Integer> nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            int delta = 2020 - nums.get(i);
+            if(map.containsKey(delta)) {
+                ans = map.getOrDefault(i, delta) * nums.get(i);
+            } else {
+                map.put(nums.get(i), i);
+            }
+        }
+        return ans;
+    }
+
+    // brute force algorithms
+    public static int DayOnePartOne(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == 2020) {
+                    res = nums[i] * nums[j];
+                }
+            }
+        }
+        return res;
+    }
+
+    public static int DayOnePartTwo(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 2020) {
+                        res = nums[i] * nums[j] * nums[k];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ day2.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+    1-3 a: abcde
+    1-3 b: cdefg
+    2-9 c: ccccccccc
+*/
+
+public class day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        while (sc.hasNext()) {
+            String x = sc.nextLine();
+            Pattern p = Pattern.compile("([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)");
+            Matcher m = p.matcher(x);
+            String lower = "", upper = "", letter = "", password = "";
+            while (m.find()) {
+                lower = m.group(1);
+                upper = m.group(2);
+                letter = m.group(3);
+                password = m.group(4);
+            }
+            ans += isValid(Integer.parseInt(lower), Integer.parseInt(upper), letter, password);
+        }
+        sc.close();
+        System.out.println(ans);
+    }
+
+    public static int isValid(int lower, int upper, String letter, String password) {
+        int count = 0;
+        int totalValid = 0;
+        char c = letter.charAt(0);
+        for (int i = 0; i < password.length(); i++) {
+            if (password.charAt(i) == c) {
+                count += 1;
+            }
+        }
+        if (count >= lower && count <= upper) {
+            totalValid++;
+        }
+        return totalValid;
+    }
+
+    public static int isValidPartTwo(int lower, int upper, String letter, String password) {
+        char c = letter.charAt(0);
+        int count = 0;
+        Boolean lowerFlag = false, upperFlag = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (i + 1 == lower) {
+                if (password.charAt(i) == c) {
+                    lowerFlag = true;
+                }
+            } else if (i + 1 == upper) {
+                if (password.charAt(i) == c) {
+                    upperFlag = true;
+                }
+            }
+        }
+        if (lowerFlag || upperFlag) {
+            count += 1;
+        } else if (lowerFlag && upperFlag) {
+            count -= 1;
+        }
+        return count;
+    }
+
+}
